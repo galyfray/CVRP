b449070d5c628a421eee75a6a80be24ffcd36461
nexowo <jmaccou@outlook.fr>
2022-11-21 22:02:37 +0100

    Completion of the previous commit

--- src/cvrp/attention.py
+++ src/cvrp/attention.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Generator
 
 class Attention(ABC):
     """
@@ -7,10 +6,10 @@ class Attention(ABC):
     """
 
     @abstractmethod
-    def attention(v: list) -> Generator: #Les vecteurs sont représentés comme des listes mais ça peut être changé
+    def attention(v: list) -> list: #Les vecteurs sont représentés comme des listes mais ça peut être changé
         """
         :param v: a vector on which we want to execute the attention mecanism
-
-        :return: a Generator TODO define the type and what do the generator
+        
+        :return: a vector of the probabilities to compute the learning on each element of the vector
         """
         pass
