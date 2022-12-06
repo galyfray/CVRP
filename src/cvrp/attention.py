6370ad82ad27a903a70905121dda24bd276ece48
nexowo <jmaccou@outlook.fr>
2022-12-06 14:26:51 +0100

    Beginning of attention class implementation

--- src/cvrp/attention.py
+++ src/cvrp/attention.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 import tensorflow as tf
-from tensorflow import keras
 from keras import layers
 
 class Attention(ABC):
@@ -9,7 +8,7 @@ class Attention(ABC):
     """
 
     @abstractmethod
-    def attention(self, v: list) -> list: #Les vecteurs sont représentés comme des listes mais ça peut être changé
+    def attention(self, v: tf.Tensor) -> list: #Les vecteurs sont représentés comme des listes mais ça peut être changé
         """
         :param v: a vector on which we want to execute the attention mecanism
         
@@ -27,14 +26,21 @@ class AttentionFactory(ABC):
         pass
 
 
-class AttentionClass(Attention):
-    def __init__(self):
+class AttentionClass(Attention, tf.keras.Model):
+    def __init__(self, n:int):
         """
         """
-        pass
+        super(AttentionClass,self).__init__(name = '')
+        self._v = layers.Dense(64, activation = "tanh")
+        self._a = layers.Dense(64, activation = "softmax")
+        self._g = layers.Dense(64, activation = "tanh")
+        self._p = layers.Dense(64, activation = "softmax")
+        self._n = n
+        self.training = True
 
-    def _context_vector(self):
-        pass
-
-    def _a(self, v : list) -> list:
-        pass
+    def _context_vector(self, mu : tf.Tensor, h : tf.Tensor):
+        value = 0
+        for i in range(self._n + 1):
+            v_i = self._v(mu + h)
+            a_i = self._a(v_i)
+            value+=a_i*mu[i]
