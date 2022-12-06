8d38aaa35d4274ce1aacdf8c979b504dd8c86b2c
nexowo <jmaccou@outlook.fr>
2022-12-06 17:42:56 +0100

    License

--- src/cvrp/attention.py
+++ src/cvrp/attention.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module holds the various constraint validators used to configure the ECVRP
+
+@author: Jean Maccou
+@license: GPL-3
+@date: 2022-12-06
+@version: 0.1
+"""
+
+# CVRP
+# Copyright (C) 2022  A.Marie, K.Sonia, M.Jean, O.Cyril, V.Axel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from abc import ABC, abstractmethod
 import tensorflow as tf
 from keras import layers
