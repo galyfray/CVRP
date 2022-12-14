f77b0f7697d4d8dc065ede92c6352faf68dd0909
Marie <71456033+m-aspro@users.noreply.github.com>
2022-12-14 11:13:16 +0100

    Mutate and crossover operators (#25)
    Firstly, this PR also contains the work of the PR
    https://github.com/galyfray/CVRP/pull/12 made by @galyfray
    I highly recommand you to see his work before mine.
    
    ## What has been done 
    
    
    ### Crossover on the ecvrp file:
    #### Creation of the method crossover, include other methods:
    - **remove_point** (_private_) : remove a precised point in a solution
    for the crossover process
    - **best_place_for** (_public_) : find the best place to add a point in
    a new road at the end of the crossover process
    
    
    ### Mutation on the ecvrp file:
    #### Creation of the method mutation, include other methods:
    - **two_opt** (_private_): algorithm which find the best mutation for
    this road by calculating the distance for each mutation of road
    - **reversed_content** (_public_): reverse the content of a road between
    the point 1 and 2 (included)
    
    ### Methods used for both:
    - **tuple_to_list** (_private_): convert a tuple[tuple[int] into a
    list[list[int]] because tuple are immutable
    - **pull_off_chargers** (_private_) : remove all the chargers from a
    solution
    - **road_correction** (_protected_): add the closest electric charge
    between 2 points
    - **closest_charger** (_private_): find the closest charger of a point
    - **delete_empty_road** (_public_): delete empty road which are
    represented by [0, 0]
    - **merge_roads** (_private_): merge roads to obtain a solution in a
    simple list
    
    ### Test on the test_ecvrp file:
    - A new instance has been created to test new methods
    - Crossover has been tested
    - Mutation has been tested
    - Empty road and road correction have been tested to cover some tricky
    cases
    
    ## What has not been done
    - Unit tests for each method
    - Recreating a situation where the best place is to add a road if all
    vehicles are not monopolized, it would cover the lines 165-166, 182, and
    282-283
    
    ## Coverage
    
    Name | Stmts | Miss |  Cover | Missing
    |-|-|-|-|-|
    src\cvrp\ecvrp.py  | 273 | 4 | 98%  | 165-166, 182, 282-283
    TOTAL | 616 | 4  |  99% |
    
    Co-authored-by: galyfray <37240229+galyfray@users.noreply.github.com>
    Co-authored-by: Galyfray <galyfray@gmail.com>

---
+++ src/cvrp/json_io.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module holds the IO handlers for our data storage.
+
+@author: Cyril Obrecht
+@license: GPL-3
+@date: 2022-11-02
+@version: 0.1
+"""
+
+# CVRP
+# Copyright (C) 2022  A.Marie, K.Sonia, M.Jean, O.Cyril, V.Axel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+from bz2 import BZ2File
+from typing import Union
+import json
+
+from .ecvrp import ECVRPSolution
+
+
+class JsonWriter:
+    """
+    Class handling the writing of information to a specific JSON file with pre defined structure.
+
+    In order to limit the space cost of those files they are compressed with bz2.
+    """
+
+    def __init__(
+                self,
+                root: str,
+                name: str,
+                bench_name: str
+            ):
+        """
+        Initialize the JsonWriter class.
+
+        :param root: Base folder to save files to.
+        :param name: Name of the file to store the data in.
+        :param bench_id: The unique ID of the benchmark that produced this result.
+        """
+        self.__snapshots = []
+        self.__root = Path(root)
+        self.__name = name
+        self.__bench_name = bench_name
+
+    def add_snapshot(self, snapshot: list[ECVRPSolution], time: float) \
+            -> dict[str, Union[float, list[dict[str, Union[int, tuple[int, ...]]]]]]:
+        """
+        Add a snapshot to the structure.
+
+        The snapshots are dumped in the same order as provided.
+        Hence if you want to retrosctively add new snapshots between existing,
+        make sure that you handle this at read time.
+        """
+        generation = {"time": time, "individuals": []}
+
+        for individual in snapshot:
+            generation["individuals"].append({
+                "solution": individual.get_points(),
+                "fitness": individual.get_fitness()
+            })
+
+        self.__snapshots.append(generation)
+
+        return generation
+
+    def dump(self):
+        """
+        Dump all the collected data to the file name `root/name.json.bz2`.
+
+        This method will override any existing file without warning.
+        """
+        data = {
+            "bench_id": self.__bench_name,
+            "snapshots": self.__snapshots
+        }
+        with BZ2File(str(self.__root.joinpath(f"{self.__name}.json.bz2")), "wb") as file:
+            dump = json.dumps(data, separators=(",", ":")).encode("U7")
+            file.write(dump)
+            file.close()
+
+
+def read_json(root: str, name: str) -> dict[str, any]:
+    """Read a JSON file and recover snapshots."""
+    root = Path(root)
+    data: bytes
+
+    with BZ2File(str(root.joinpath(f"{name}.json.bz2")), "rb") as file:
+        data = file.read()
+        file.close()
+
+    return json.loads(data.decode("U7"))
